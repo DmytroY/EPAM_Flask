488ca7d58beccfbd7a4eb33e7adcb972e08b3ac9
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2023-02-14 13:45:51 +0200

    sync

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+
+db = SQLAlchemy(app)
+class students(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))  
+   addr = db.Column(db.String(200))
+   pin = db.Column(db.String(10))
+
+def __init__(self, name, city, addr,pin):
+   self.name = name
+   self.city = city
+   self.addr = addr
+   self.pin = pin
+
+db.create_all()
 
 # Configuring MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'tutorial'
-mysql = MySQL(app)
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'password'
+# app.config['MYSQL_DB'] = 'tutorial'
+# mysql = MySQL(app)
 
-@app.route('/')
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv = cur.fetchall()
-    return str(rv)
 
-# @app.route("/")
+@app.route("/")
+def show_all():
+   return render_template('show_all.html', students = students.query.all() )
+
 # def index():
-
 #     return render_template("index.html")
 
 # @app.route("/response", methods=["POST"])
