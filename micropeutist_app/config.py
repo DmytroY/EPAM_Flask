433dd0f5cc6a4a25921a7d7b6cf42ab90b4d0625
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2023-02-20 21:49:49 +0200

    cleared mysql pass fot travis

--- micropeutist_app/config.py
+++ micropeutist_app/config.py
@@ -5,7 +5,7 @@ import os
 #     SECRET_KEY = "secret_string_bla-bla-bla"
 
 #     # configure DB connection
-#     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysqlpass@localhost/micropeutist'
+#     SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/micropeutist'
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 from flask import Flask
@@ -13,7 +13,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysqlpass@localhost/micropeutist'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/micropeutist'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
