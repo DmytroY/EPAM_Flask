e98abb703d7f7b8af7989865f1754d9ac9e4e6ec
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2023-02-21 17:01:10 +0200

    pylint added to travis.yml

--- micropeutist_app/application.py
+++ micropeutist_app/application.py
@@ -1,10 +1,8 @@
+'''assemble application'''
+import EPAM_Flask.micropeutist_app.views.web_view # pylint: disable=unused-import
+import EPAM_Flask.micropeutist_app.rest.api_view # pylint: disable=unused-import
 from .config import app
-from .models.model import *
 
-import EPAM_Flask.micropeutist_app.views.web_view
-
-import EPAM_Flask.micropeutist_app.rest.api_view
-import EPAM_Flask.micropeutist_app.views.web_view
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- micropeutist_app/config.py
+++ micropeutist_app/config.py
@@ -1,12 +1,5 @@
+''' Configuration and instansiate Flask and Database'''
 import os
-
-# class Config(object):
-#     # SECRET_KEY = os.environ.get(SECRET_KEY) or "secret_string_bla-bla-bla"
-#     SECRET_KEY = "secret_string_bla-bla-bla"
-
-#     # configure DB connection
-#     SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/micropeutist'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 from flask import Flask
 from flask_migrate import Migrate
@@ -15,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/micropeutist'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
- 
+
 db = SQLAlchemy(app)
 
 MIGRATION_DIR = os.path.join('micropeutist_app', 'migrations')
